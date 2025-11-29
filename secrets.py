79cea7fdffa3389d9d1aca3d029ae5f420a0f03a
bluepearlws <bluepearlws@gmail.com>
2025-11-29 11:56:57 -0500

    Remove AWS IAM user data from secrets.py
    Removed sensitive AWS IAM user information from secrets.py.

--- secrets.py
+++ secrets.py
@@ -1,18 +1,3 @@
 # Test secrets file
 TWILIO_API_KEY = "SKXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
 STRIPE_SECRET_KEY = "sk_live_XXXXXXXXXXXXXXXXXXXXXXXX"
-
-──(kali㉿kali)-[~/iscan-tool]
-└─$ aws iam list-users               
-{
-    "Users": [
-        {
-            "Path": "/",
-            "UserName": "cli-user",
-            "UserId": "AIDA46HVBEZTBOYHJFL5A",
-            "Arn": "arn:aws:iam::889572107878:user/cli-user",
-            "CreateDate": "2025-09-10T11:13:27+00:00",
-            "PasswordLastUsed": "2025-09-10T12:27:38+00:00"
-        }
-    ]
-}
